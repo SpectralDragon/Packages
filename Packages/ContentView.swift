4c1db149915f841bbe156fdaf80191cde697ca11
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2020-02-16 18:27:11 +0300

    Initial Commit

---
+++ Packages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Packages
+//
+//  Created by Vladislav Prusakov on 16.02.2020.
+//  Copyright © 2020 LiteCode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PackagesTests/PackagesTests.swift
@@ -0,0 +1,34 @@
+//
+//  PackagesTests.swift
+//  PackagesTests
+//
+//  Created by Vladislav Prusakov on 16.02.2020.
+//  Copyright © 2020 LiteCode. All rights reserved.
+//
+
+import XCTest
+@testable import Packages
+
+class PackagesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
